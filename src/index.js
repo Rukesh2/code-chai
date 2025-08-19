ad45e32b2d545dd4e04ba515a5c875bb2d34af0b
Rukesh2 <rukeshruk1991@gmail.com>
2025-08-19 20:33:58 +0530

    setup project files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import 'dotenv/config'
+
+const app = express();
+
+app.get('/',(req,res)=>{
+  res.send(`server is running`);
+})
+
+app.listen(process.env.PORT,()=>{
+  console.log(`Server is running successfully`);
+})
